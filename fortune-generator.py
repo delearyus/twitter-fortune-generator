93e8f45f4934d055ede8d1b11c757a6a38b9f971
jamie <jmleary@willamette.edu>
2017-04-20 14:17:16 -0700

    added arg parsing and flags

--- fortune-generator.py
+++ fortune-generator.py
@@ -5,6 +5,7 @@ import time
 import shutil
 from subprocess import call
 import os
+import argparse
 
 try:
     import keys
@@ -18,13 +19,29 @@ except ImportError:
     print("python-twitter not found, install with pip install python-twitter")
     sys.exit(1)
 
-#-----------------------------------------------------------------------------#
+#----[ Privelege Check ]------------------------------------------------------#
 
 if os.geteuid() != 0:
     print("This script needs to be run as root.")
     sys.exit(1)
 
-#-----------------------------------------------------------------------------#
+#----[ Arg parsing ]----------------------------------------------------------#
+
+parser = argparse.ArgumentParser()
+parser.add_argument("screen_name",
+                    help="Twitter handle of user to pull from")
+
+parser.add_argument("-l","--nolinks",
+                    help="filter out tweets with links",
+                    action="store_true")
+
+parser.add_argument("-m","--nomentions",
+                    help="filter out tweets with mentions",
+                    action="store_true")
+
+args = parser.parse_args()
+
+#----[ Api initialization ]---------------------------------------------------#
 
 api = twitter.Api(consumer_key=keys.consumer_key,
                   consumer_secret=keys.consumer_secret,
@@ -32,12 +49,12 @@ api = twitter.Api(consumer_key=keys.consumer_key,
                   access_token_secret=keys.access_secret)
 
 statuses = set()
-screen_name = 'dril'
+screen_name = args.screen_name
 
 temp = ["placeholder"]
 oldestid = 1000000000000000000
 
-#-----------------------------------------------------------------------------#
+#----[ Api call loop ]--------------------------------------------------------#
 
 while (len(temp) > 0):
 
@@ -60,31 +77,35 @@ print("finished!")
 
 print("Found a total of {} unique statuses".format(len(statuses)))
 
-#-----------------------------------------------------------------------------#
+#----[ Filtering ]------------------------------------------------------------#
 
-filteredstatuses = {x for x in statuses if "http" not in x}
+if args.nolinks:
+    filteredstatuses = {x for x in statuses if "http" not in x}
 
-text = "Filtered out {} statuses with embedded links"
-print(text.format(len(statuses) - len(filteredstatuses)))
+    text = "Filtered out {} statuses with embedded links"
+    print(text.format(len(statuses) - len(filteredstatuses)))
 
-statuses = filteredstatuses
+    statuses = filteredstatuses
 
-filteredstatuses = {x for x in statuses if "@" not in x}
+if args.nomentions:
+    filteredstatuses = {x for x in statuses if "@" not in x}
 
-text = "Filtered out {} statuses with mentions"
-print(text.format(len(statuses) - len(filteredstatuses)))
+    text = "Filtered out {} statuses with mentions"
+    print(text.format(len(statuses) - len(filteredstatuses)))
+    
+    statuses = filteredstatuses
 
-#-----------------------------------------------------------------------------#
+#----[ File Output ]----------------------------------------------------------#
 
 datfile = open(screen_name, "w")
 
-for i in filteredstatuses:
+for i in statuses:
     datfile.write(i)
     datfile.write("\n%\n")
 
 datfile.close()
 
-#-----------------------------------------------------------------------------#
+#----[ Datfile Generation and Installation ]----------------------------------#
 
 call(["strfile", "-c", "%", datfile.name, datfile.name + ".dat"])
 
